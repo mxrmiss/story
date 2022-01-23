872bb6fe84c96c6e2d190fb2013a6b1be8f55bfc
mxrmiss <heroli520@outlook.com>
2022-01-24 00:51:10 +0800

    update

--- ackup/blog_daily.sh
+++ ackup/blog_daily.sh
@@ -3,6 +3,8 @@
 # hexo.sh 的作用是发布博客
 # 该脚本位于/etc/cron.daily目录，作用是每天更新博客,每周末清除日志内容，包括错误和正确的日志
 
+echo "loading..."
+
 if [ $(date +%u) -eq 7 ]
 then
     echo "" > /root/story/blog_daily.log
@@ -13,12 +15,41 @@ exec 2>>/root/story/blog_daily_error.log
 echo "=============****错误日志****===============" >&2 
 date >&2; echo "" >&2
 
+exec 3>&1
 exec 1>>/root/story/blog_daily.log
 echo "=============*****运行日志*****================"
 date;echo ""
+
 
 cd /root/story
 git pull
 cd /root/story/blog
 hexo clean
 hexo g -d
+cd /www/wwwroot/oldstory.cn/public/
+
+for i in `ls -a`
+do 
+    if [ $i != .user.ini ] && [ $i != . ] && [ $i != .. ];then
+        rm -rf $i
+    fi
+done
+
+cp -r /root/story/blog/public/* ./
+
+
+cd /root/story/life
+hexo clean
+hexo g -d
+cd /www/wwwroot/life.oldstory.cn/public/
+
+for i in `ls -a`
+do 
+    if [ $i != .user.ini ] && [ $i != . ] && [ $i != .. ];then
+        rm -rf $i
+    fi
+done
+
+cp -r /root/story/life/public/* ./
+exec 1>&3
+echo "loading successfully!"
